e2dd2ababe9e5bab64569130173a33ae15e8fb91
msidqi01 <sidqiindodax@gmail.com>
2026-02-21 21:36:36 +0700

    Implement NotifAccess module with permission methods

--- plugins/notifaccess/www/notifaccess.js
+++ plugins/notifaccess/www/notifaccess.js
@@ -1,3 +1,15 @@
-cordova.define("notifaccess.NotifAccess", function (require, exports, module) {
-  module.exports = {};
-});
+var exec = require('cordova/exec');
+
+var NotifAccess = {
+  // buka halaman settings notification access
+  requestPermission: function (success, error) {
+    exec(success, error, 'NotifAccess', 'requestPermission', []);
+  },
+
+  // cek apakah service notification listener di-enable
+  isEnabled: function (success, error) {
+    exec(success, error, 'NotifAccess', 'isEnabled', []);
+  }
+};
+
+module.exports = NotifAccess;
