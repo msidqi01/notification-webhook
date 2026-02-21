9fb61cbb0f8d2e77bd3797f7c426024b537f336f
msidqi01 <sidqiindodax@gmail.com>
2026-02-21 21:11:57 +0700

    Update NotifAccess.java

--- plugins/notifaccess/src/android/NotifAccess.java
+++ plugins/notifaccess/src/android/NotifAccess.java
@@ -1,42 +1,40 @@
-package com.yourapp.notifaccess;
+package com.notifaccess;
 
+import android.content.Context;
 import android.content.Intent;
 import android.provider.Settings;
 import android.text.TextUtils;
 
-import org.apache.cordova.*;
-import org.json.JSONArray;
-import org.json.JSONException;
+import org.apache.cordova.CordovaPlugin;
 
 public class NotifAccess extends CordovaPlugin {
 
-  @Override
-  public boolean execute(String action, JSONArray args, CallbackContext cb) throws JSONException {
-    if ("isEnabled".equals(action)) {
-      cb.success(isNotificationServiceEnabled() ? 1 : 0);
-      return true;
+    @Override
+    protected void pluginInitialize() {
+        try {
+            Context ctx = cordova.getContext();
+            if (!isNotificationListenerEnabled(ctx)) {
+                // Buka layar akses notifikasi (Notification Listener)
+                Intent intent = new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                ctx.startActivity(intent);
+            }
+        } catch (Exception ignored) {
+        }
     }
-    if ("openSettings".equals(action)) {
-      openNotificationAccessSettings();
-      cb.success(1);
-      return true;
+
+    private boolean isNotificationListenerEnabled(Context context) {
+        try {
+            String enabledListeners = Settings.Secure.getString(
+                    context.getContentResolver(),
+                    "enabled_notification_listeners"
+            );
+            if (TextUtils.isEmpty(enabledListeners)) return false;
+
+            // format string biasanya: "com.app/.ServiceName:com.app2/.Service2"
+            return enabledListeners.contains(context.getPackageName());
+        } catch (Exception e) {
+            return false;
+        }
     }
-    return false;
-  }
-
-  private boolean isNotificationServiceEnabled() {
-    String pkgName = cordova.getActivity().getPackageName();
-    final String flat = Settings.Secure.getString(
-        cordova.getActivity().getContentResolver(),
-        "enabled_notification_listeners"
-    );
-    if (TextUtils.isEmpty(flat)) return false;
-    return flat.contains(pkgName);
-  }
-
-  private void openNotificationAccessSettings() {
-    Intent intent = new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS);
-    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    cordova.getActivity().startActivity(intent);
-  }
 }
