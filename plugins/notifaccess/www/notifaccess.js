51e49fb313ede479ba5e992f77df4a4be01e8ddb
msidqi01 <sidqiindodax@gmail.com>
2026-02-21 21:53:09 +0700

    Refactor NotifAccess methods to use exports

--- plugins/notifaccess/www/notifaccess.js
+++ plugins/notifaccess/www/notifaccess.js
@@ -1,15 +1,10 @@
+/* global cordova */
 var exec = require('cordova/exec');
 
-var NotifAccess = {
-  // buka halaman settings notification access
-  requestPermission: function (success, error) {
-    exec(success, error, 'NotifAccess', 'requestPermission', []);
-  },
-
-  // cek apakah service notification listener di-enable
-  isEnabled: function (success, error) {
-    exec(success, error, 'NotifAccess', 'isEnabled', []);
-  }
+exports.requestPermission = function (success, error) {
+  exec(success, error, 'NotifAccess', 'requestPermission', []);
 };
 
-module.exports = NotifAccess;
+exports.isEnabled = function (success, error) {
+  exec(success, error, 'NotifAccess', 'isEnabled', []);
+};
